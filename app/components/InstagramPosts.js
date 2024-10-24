1afc2361946b200e1185d3b4077d50acb88c9fcb
Omar ♔ <o_yousaf@live.co.uk>
2024-10-24 14:30:14 +0100

    squashing bugs

--- app/components/InstagramPosts.js
+++ app/components/InstagramPosts.js
@@ -12,7 +12,7 @@ const InstagramPosts = () => {
   return (
     <section className="py-8" id="instagram">
       <h2 className="text-4xl font-bold text-center mb-8">Latest Cars</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-6xl mx-auto text-center px-4 lg:px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto text-center px-4 lg:px-8">
         {posts.map((post) => (
           <div key={post.id} className="bg-rose-800 hover:bg-rose-600 p-4 shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out">
             <h3 className="text-xl font-bold">{post.title}</h3>
--- app/constants/index.js
+++ app/constants/index.js
@@ -51,16 +51,48 @@ export const reviews = [
   {
     name: "Donna Rose",
     feedback:
-      "Went to go have a look at a audi a1 with my husband and father in law it was in great condition,  great price , the service we were given was outstanding very polite, helpful to the point my father in law didn't come with the intention of buying a new car but we came away with 2 highly recommend couldn't do enough for us , had the cars around 7 months and no problems",
+      "Went to go have a look at an Audi A1 with my husband and father-in-law. Car was in great condition, great price, and the service we were given was outstanding. Very polite & helpful - to the point my father-in-law didn't come with the intention of buying a new car but we came away with 2! Highly recommend - couldn't do enough for us! Had the cars around 7 months and so far so good; no problems whatsoever!",
   },
   {
     name: "Tom Betts",
-    feedback: `Highly recomend. I purchased a bmw 320d. it was well presented, came fully serviced and valeted. The car came with a 3 month warranty and just under 3 months i came across a fault that was taken care of with a local garage. Paid with no hassle by ace motor sales. Would definitely buy from these again. 👍🏻`,
+    feedback: `Highly recommend. I purchased a BMW 320d. It was well presented, came fully serviced and valeted. The car also came with a 3 month warranty and just under 3 months I came across a fault that was taken care of with a local garage. Paid with no hassle by Ace Motor Sales. Would definitely buy from them again. 👍🏻`,
   },
   {
     name: "VEYGO GROUPS",
     feedback:
-      "Brilliant customer service by fez! Brilliant deals lovely motors get your selves down!!!",
+      "Brilliant customer service by Fez! Brilliant deals; lovely motors get your selves down!!!",
+  },
+  {
+    name: "Eddie Kahoro",
+    feedback:
+      "Very professional, helpful and genuine. Made sure that all our questions were answered before we chose to buy. Very reliable and polite.",
+  },
+  {
+    name: "Julie Longworth",
+    feedback:
+      "An absolute gem of used car dealer. Purchased my fabulous Nissan Juke 3 weeks ago and the service received from Ace Motors has been excellent. Had a little issue with the air-con not working but was quickly sorted. Many thanks for making this buying experience stress free and a pleasure, can recommend with confidence.",
+  },
+  {
+    name: "Adil",
+    feedback: `Recently bought an Audi a3 from Ace motor sales. Great vehicle at a great price and was a very easy process buying the car.
+Staff were very accommodating and friendly and went out of their way to help me with anything i needed.
+Highly recommend buying a car from here and i will definitely be purchasing another in the future`,
+  },
+  {
+    name: "khaalid mulla",
+    feedback: `Great vehicle and exactly as described at a very reasonable price. Fantastic service and vehicle was ready to go. A very smooth process with the paperwork and an overall great purchasing experience with helpful staff.
+
+Definitely will be returning to purchase another vehicle in the future. Best of luck to the owner`,
+  },
+  {
+    name: "Donna Scott",
+    feedback: `Brilliant service! Car was delivered to my door the day after I enquired and I was given a good deal on my part-ex.
+Thank you so much for everything! Highly recommended 5*`,
+  },
+  {
+    name: "Denisa",
+    feedback:
+      "Good car and good price. Nice staff too, helpful with everything. Even came out on a Sunday for us to buy the car",
   },
 ];
 
@@ -74,7 +106,9 @@ export const mockData = [
         {`
           The all-electric Taycan Turbo S with Performance Battery Plus, high driving dynamics, and a flat flyline.
         `}
-        <span className="block mt-3 font-bold text-xl">Available from £161,400</span>
+        <span className="block mt-3 font-bold text-xl">
+          Available from £161,400
+        </span>
       </>
     ),
   },
@@ -87,7 +121,9 @@ export const mockData = [
         {`
           The Hyundai IONIQ 5 is a fully-electric midsize CUV with 800V battery technology for ultra-fast charging and offers a driving range of up to 354 miles.
         `}
-        <span className="block mt-3 font-bold text-xl">Available from £39,900</span>
+        <span className="block mt-3 font-bold text-xl">
+          Available from £39,900
+        </span>
       </>
     ),
   },
@@ -100,7 +136,9 @@ export const mockData = [
         {`
           Model Y is a fully electric, mid-size SUV with unparalleled protection and versatile cargo space.
         `}
-        <span className="block mt-3 font-bold text-xl">Available from £44,990</span>
+        <span className="block mt-3 font-bold text-xl">
+          Available from £44,990
+        </span>
       </>
     ),
   },
